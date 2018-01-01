ac5c4d16e38990e3fd974565180b01afe2a2ae7c
WalidRH <33457801+WalidRH@users.noreply.github.com>
2018-01-01 20:32:36 +0000

    antenna

---
+++ projetantenne/projetantenne/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace projetantenne
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.map = new SharpMap.Forms.MapBox();
+            this.MyMap = new SharpMap.Forms.MapBox();
+            this.lbl = new System.Windows.Forms.Label();
+            this.X = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Y = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // map
+            // 
+            this.map.ActiveTool = SharpMap.Forms.MapBox.Tools.None;
+            this.map.Cursor = System.Windows.Forms.Cursors.Default;
+            this.map.FineZoomFactor = 10D;
+            this.map.Location = new System.Drawing.Point(12, 114);
+            this.map.MapQueryMode = SharpMap.Forms.MapBox.MapQueryType.LayerByIndex;
+            this.map.Name = "map";
+            this.map.QueryGrowFactor = 5F;
+            this.map.QueryLayerIndex = 0;
+            this.map.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(244)))), ((int)(((byte)(244)))), ((int)(((byte)(244)))));
+            this.map.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(244)))), ((int)(((byte)(244)))));
+            this.map.ShowProgressUpdate = false;
+            this.map.Size = new System.Drawing.Size(565, 294);
+            this.map.TabIndex = 0;
+            this.map.Text = "mapBox1";
+            this.map.WheelZoomMagnitude = -2D;
+            // 
+            // MyMap
+            // 
+            this.MyMap.ActiveTool = SharpMap.Forms.MapBox.Tools.None;
+            this.MyMap.Cursor = System.Windows.Forms.Cursors.Default;
+            this.MyMap.FineZoomFactor = 10D;
+            this.MyMap.Location = new System.Drawing.Point(105, 12);
+            this.MyMap.MapQueryMode = SharpMap.Forms.MapBox.MapQueryType.LayerByIndex;
+            this.MyMap.Name = "MyMap";
+            this.MyMap.QueryGrowFactor = 5F;
+            this.MyMap.QueryLayerIndex = 0;
+            this.MyMap.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(244)))), ((int)(((byte)(244)))), ((int)(((byte)(244)))));
+            this.MyMap.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(244)))), ((int)(((byte)(244)))));
+            this.MyMap.ShowProgressUpdate = false;
+            this.MyMap.Size = new System.Drawing.Size(447, 370);
+            this.MyMap.TabIndex = 1;
+            this.MyMap.Text = "mapBox1";
+            this.MyMap.WheelZoomMagnitude = -2D;
+            this.MyMap.MouseDown += new SharpMap.Forms.MapBox.MouseEventHandler(this.MyMap_MouseDown);
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Location = new System.Drawing.Point(12, 398);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(35, 13);
+            this.lbl.TabIndex = 2;
+            this.lbl.Text = "label1";
+            // 
+            // X
+            // 
+            this.X.AutoSize = true;
+            this.X.Location = new System.Drawing.Point(12, 12);
+            this.X.Name = "X";
+            this.X.Size = new System.Drawing.Size(14, 13);
+            this.X.TabIndex = 3;
+            this.X.Text = "X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 218);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 192);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // Y
+            // 
+            this.Y.AutoSize = true;
+            this.Y.Location = new System.Drawing.Point(12, 43);
+            this.Y.Name = "Y";
+            this.Y.Size = new System.Drawing.Size(14, 13);
+            this.Y.TabIndex = 6;
+            this.Y.Text = "Y";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 420);
+            this.Controls.Add(this.Y);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.X);
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.MyMap);
+            this.Controls.Add(this.map);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private SharpMap.Forms.MapBox map;
+        private SharpMap.Forms.MapBox MyMap;
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.Label X;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Y;
+    }
+}
+
---
+++ projetantenne/projetantenne/Form1.cs
@@ -0,0 +1,99 @@
+﻿using GeoAPI.Geometries;
+using SharpMap.Data;
+using SharpMap.Data.Providers;
+using SharpMap.Layers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Npgsql;
+
+namespace projetantenne
+{
+    public partial class Form1 : Form
+    {
+        string cnx = "server=localhost;port=5432;database=project;username=postgres;pwd=root";
+        PostGIS DataQuartier;
+        PostGIS DataAntenne;
+        VectorLayer LayerQuartier;
+        VectorLayer Layerantenne;
+        VectorLayer LayerSelection;
+        public Form1()
+        {
+            InitializeComponent();
+            DataQuartier = new PostGIS(cnx, "quartier", "id");
+            DataAntenne = new PostGIS(cnx, "antenne", "code_a");
+            LayerQuartier = new VectorLayer("qrt", DataQuartier);
+            LayerQuartier.Style.Fill = Brushes.Green;
+            LayerQuartier.Style.Outline = new Pen(Color.Black, 1);
+            LayerQuartier.Style.EnableOutline = true;
+            LayerSelection = new VectorLayer("selection");
+            LayerSelection.Style.Fill = Brushes.Yellow;
+            Layerantenne = new VectorLayer("ant", DataAntenne);
+            MyMap.Map.Layers.Add(LayerQuartier);
+            MyMap.Map.Layers.Add(Layerantenne);
+            MyMap.Map.ZoomToExtents();
+            MyMap.Refresh();
+        }
+
+         private void MyMap_MouseDown(GeoAPI.Geometries.Coordinate worldPos, MouseEventArgs imagePos)
+        {
+            //FeatureDataSet ds = new FeatureDataSet();//a dataset that exists only in a geodatabase workspace 
+            //Envelope box = new Envelope(worldPos);// represent the bounding box of a Geometry, e.g. the minimum and maximum x and y values of the Coordinates
+            //box.ExpandBy(0.001);
+            //Layerantenne.ExecuteIntersectionQuery(box, ds);
+            //FeatureDataRow row = ds.Tables[0][0];
+            //lbl.Text = string.Format("{0} , {1}", row[0], row[1]);
+            //// Console.WriteLine("X: {0} , Y: {1}", Cursor.Position.X, Cursor.Position.Y);
+            // Console.WriteLine("{0} , {1}", row[0], row[1]);
+            ////.OSGeo.OGR.DataSource = new GeometryProvider(row);
+            ////MyMap.Map.Layers.Add( );
+
+
+            //MyMap.Refresh();
+            List<double> coordx = new List<double>();
+            List<double> coordy = new List<double>();
+            List<double> rayon = new List<double>();
+
+            NpgsqlConnection conn = new NpgsqlConnection(cnx);
+            conn.Open();
+            NpgsqlCommand command = new NpgsqlCommand("SELECT ST_X(position), ST_Y(position), ST_AsText(position),rayon,code_a  FROM antenne", conn);
+
+            NpgsqlDataReader dr = command.ExecuteReader();
+
+            while (dr.Read())
+            {
+                coordx.Add((Convert.ToDouble(dr[0])));
+                coordy.Add((Convert.ToDouble(dr[1])));
+                rayon.Add((Convert.ToDouble(dr[3])));
+
+            }
+            conn.Close();
+
+            X.Text = "x:" + worldPos.X.ToString();
+            Y.Text = "y:" + worldPos.X.ToString();
+
+
+
+            for (int i = 0; i < rayon.Count(); i++)
+            {
+               
+                double dis = Math.Sqrt(Math.Pow((coordx[i] - worldPos.X), Convert.ToDouble(2)) + Math.Pow((coordy[i] - worldPos.Y), Convert.ToDouble(2)));
+                if (dis < rayon[i])
+                {
+                    Console.Write("\n_______________________\n" + coordx[i] + "\t" + coordy[i] +"\trayon"+rayon[i]+ "\n");
+                    label3.Text = "Vous êtes connecter à l'antenne d'identifiant" + i;
+                    label2.Text = "Antenne de rayon de " + rayon[i];
+                }
+
+
+            }
+        }
+    }
+}
